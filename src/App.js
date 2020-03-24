e3e2d7b51ffae65d66dc0edece6aa07b9fc9c6b5
Phil <phil@pcollins.tech>
2020-03-24 09:28:42 +0000

    Header added, Icons addded

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import React from 'react';
-import './assets/sass/main.scss';
-import './assets/sass/fonts.scss';
+import React from "react";
+import "./assets/sass/main.scss";
+import "./assets/sass/fonts.scss";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-
-
-
-
-import Header from './layout/Header';
-import SidebarLeft from './layout/SiderbarLeft';
-import Newsfeed from './pages/Newsfeed';
-import ProfileHeader from './assets/ProfileHeader';
-
-{/*import 'Bootstrap/dist/css/bootstrap-reboot.css';
-import 'Bootstrap/dist/css/bootstrap.css';
-import 'Bootstrap/dist/css/bootstrap-grid.css';*/}
+import Header from "./layout/Header";
+import SidebarLeft from "./layout/SiderbarLeft";
+// import Newsfeed from "./pages/Newsfeed";
+import ProfileHeader from "./components/ProfileHeader";
 
 function App() {
   return (
     <div>
-    <SidebarLeft />
-    <Header />
-    
-    <ProfileHeader />
+      <SidebarLeft />
+      <Header />
+
+      <ProfileHeader />
     </div>
-    
   );
 }
 
